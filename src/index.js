abf659e1dfaed93efbae923590e2c6db2ed9ddba
huangjingjing <huangjingjing@netposa.com>
2018-08-17 18:40:03 +0800

    update hot load

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { hot } from 'react-hot-loader';
 import registerServiceWorker from './registerServiceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Container = hot(module)(() => <App />);
+
+ReactDOM.render(<Container />, document.getElementById('root'));
 registerServiceWorker();
